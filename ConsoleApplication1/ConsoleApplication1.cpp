2ac9ec47e3143b1a5cd739c84daf2c71d587e303
20172104261 <925512285@qq.com>
2018-06-04 09:51:57 +0800

    jicheng
    修改了一些

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,63 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class CDate
+{
+protected:
+	int year;
+	int month;
+	int day;
+public:
+
+	CDate();
+	CDate(int ty, int tm, int td);
+	void display();
+	~CDate();
+};
+
+CDate::CDate()
+{
+	year = 2017;
+	month = 12;
+	day = 1;
+	cout << "基类默认构造函数" << year << "年" << month << "月" << day << "日" << endl;
+}
+CDate::CDate(int ty, int tm, int td)
+{
+	year = ty;
+	month = tm;
+	day = td;
+	cout << "基类构造函数" << year << "年" << month << "月" << day << "日" << endl;
+}
+void CDate::display()
+{
+	cout << "基类dispaly" << year << "年" << month << "月" << day << "日" << endl;
+}
+CDate::~CDate(void)
+{
+	cout << "基类析构函数" << year << "年" << month << "月" << day << "日" << endl;
+}
+
+class CMdate :public CDate
+{
+public:
+	CMdate(int ty,int tm,int td)
+	{
+		year = ty;
+		month = tm;
+		day = td;
+	}
+};
+
+int main()
+{
+	CMdate o(1999,1,24);
+	o.display();
+
+    return 0;
+}
+
